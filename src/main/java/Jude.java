80d3ff2a83f63ee1b089377e2f0bb408ddfc125c
Judy1x4 <e1129873@u.nus.edu>
2025-01-16 17:32:35 +0800

    Correct Java layout and add header comments

--- src/main/java/Jude.java
+++ src/main/java/Jude.java
@@ -2,16 +2,23 @@ import java.util.Scanner;
 
 public class Jude    {
     public static void main(String[] args) {
+
+        // Initialize the variables
         String name = "Jude";
-        Scanner scanner = new Scanner(System.in);
         String[] list = new String[100];
         int listSize = 0;
+        Scanner scanner = new Scanner(System.in);
 
+        // Initiate the chat
         System.out.println("Hello I'm " + name);
         System.out.println("What can I do for you, poyo?");
 
+        // Receive first user input
         String userInput = scanner.nextLine();
+
+        // Terminate input if is bye, iterate otherwise.
         while (!userInput.equals("bye")) {
+            // If the input is list, shows the list of tasks saved.
             if (userInput.equals("list")) {
                 for (int i = 0; i < listSize; i++) {
                     System.out.printf("%d. %s\n", (i + 1), list[i]);
@@ -22,6 +29,8 @@ public class Jude    {
             }
             userInput = scanner.nextLine();
         }
+        
+        // Terminate the chat
         System.out.println("Bye. Hope to see you again soon, poyo!");
         scanner.close();
     }
