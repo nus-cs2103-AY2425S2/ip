f234b5df47c8116790baad9ca761ca1a68ce1992
BladerX11 <BladerX11@proton.me>
2025-01-17 00:44:02 +0800

    Feature: Add skeletal greeting and exit

--- src/main/java/Tasker.java
+++ src/main/java/Tasker.java
@@ -1,10 +1,13 @@
 public class Tasker {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String separator = "____________________________________________________________";
+        String newLineSeparator = separator + "\n";
+        String greeting = newLineSeparator
+                + "Hello! I'm Tasker\n"
+                + "What can I do for you?\n"
+                + newLineSeparator
+                + "Bye. Hope to see you again soon!\n"
+                + separator;
+        System.out.println(greeting);
     }
 }
