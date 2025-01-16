3fda988f96bec4a5f235cdd61a26338311bb1da9
Judy1x4 <e1129873@u.nus.edu>
2025-01-16 17:31:34 +0800

    Store echoing of user input and display upon request

--- src/main/java/Jude.java
+++ src/main/java/Jude.java
@@ -4,18 +4,25 @@ public class Jude    {
     public static void main(String[] args) {
         String name = "Jude";
         Scanner scanner = new Scanner(System.in);
+        String[] list = new String[100];
+        int listSize = 0;
 
-        System.out.println("____________________________________________________________");
         System.out.println("Hello I'm " + name);
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("What can I do for you, poyo?");
 
         String userInput = scanner.nextLine();
         while (!userInput.equals("bye")) {
-            System.out.println(userInput);
+            if (userInput.equals("list")) {
+                for (int i = 0; i < listSize; i++) {
+                    System.out.printf("%d. %s\n", (i + 1), list[i]);
+                }
+            } else {
+                list[listSize++] = userInput;
+                System.out.println("added: " + userInput);
+            }
             userInput = scanner.nextLine();
         }
-
-        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon, poyo!");
+        scanner.close();
     }
 }
