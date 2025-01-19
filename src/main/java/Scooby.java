937180f193e7b91ea1ee8b41120dc12b0ebd424c
cyrolite <e1122320@u.nus.edu>
2025-01-19 17:54:32 +0800

    Rename the name from Duke to Scooby, and add greeting and exit dialogues

---
+++ src/main/java/Scooby.java
@@ -0,0 +1,17 @@
+public class Scooby {
+    // greet() greets the user by introducing its name
+    private static void greet() {
+        System.out.println("Hello! I'm Scooby");
+        System.out.println("What can I do for you?");
+    }
+
+    // exit_dialogue() exits current dialogue, and closes the bot
+    private static void exit_dialogue() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void main(String[] args) {
+        greet();
+        exit_dialogue();
+    }
+}
