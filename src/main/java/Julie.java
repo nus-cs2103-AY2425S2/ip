24379a1c43c71eb6c8105fa3683d208252161562
patil-np <nidhi.npatil@outlook.com>
2025-01-23 21:16:14 +0800

    Create task list

--- src/main/java/Julie.java
+++ src/main/java/Julie.java
@@ -1,23 +1,42 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class Julie{
+public class Julie {
+    private static final String BREAK = "______________________________________________\n";
+    private static final String INTRO = "Hello! I'm Julie\nWhat can I do for you?\n";
+    private static final String EXIT = "Bye. See you later!\n";
+    private static final ArrayList<String> taskList = new ArrayList<>();
 
-    private static final String lineBreak = "______________________________________________\n";
-    private static final String introMessage = "Hello! I'm Julie\nWhat can I do for you?\n";
-    private static final String goodbyeMessage = "Bye. See you later!\n";
+    public static void addTask(String input) {
+        taskList.add(input);
+        System.out.println(BREAK + "added: " + input + "\n" + BREAK);
+    }
+
+    public static void showTask(ArrayList<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            int label = i + 1;
+            String item = list.get(i);
+            System.out.println(label + ". " + item);
+        }
+    }
 
     public static void main(String[] args) {
-        System.out.println(lineBreak + introMessage + lineBreak);
-
+        System.out.println(BREAK + INTRO + BREAK);
 
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
 
-        while(!input.equals("bye")) {
-            System.out.println(lineBreak + input + "\n" + lineBreak);
-            input = scanner.nextLine();
+        while(true) {
+            String input = scanner.nextLine();
+            if(input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                System.out.print(BREAK);
+                showTask(taskList);
+                System.out.print(BREAK);
+            } else {
+                addTask(input);
+            }
         }
-
-        System.out.println(lineBreak + goodbyeMessage + lineBreak);
+        System.out.println(BREAK + EXIT + BREAK);
     }
 }
