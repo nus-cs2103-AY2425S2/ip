b8e291e49e7aced8098ec2a58ea75f0e87d2204d
patil-np <nidhi.npatil@outlook.com>
2025-01-23 22:53:29 +0800

    Mark and unmark

--- src/main/java/Julie.java
+++ src/main/java/Julie.java
@@ -5,19 +5,37 @@ public class Julie {
     private static final String BREAK = "______________________________________________\n";
     private static final String INTRO = "Hello! I'm Julie\nWhat can I do for you?\n";
     private static final String EXIT = "Bye. See you later!\n";
-    private static final ArrayList<String> taskList = new ArrayList<>();
+    private static final ArrayList<Task> taskList = new ArrayList<>();
 
     public static void addTask(String input) {
-        taskList.add(input);
+        Task task = new Task(input);
+        taskList.add(task);
         System.out.println(BREAK + "added: " + input + "\n" + BREAK);
     }
 
-    public static void showTask(ArrayList<String> list) {
+    public static void showTask(ArrayList<Task> list) {
+        System.out.println("Here are your tasks:");
         for (int i = 0; i < list.size(); i++) {
             int label = i + 1;
-            String item = list.get(i);
-            System.out.println(label + ". " + item);
+            Task item = list.get(i);
+            System.out.println(label + ". " + item.statusIcon() + " " + item.description);
         }
+    }
+
+    public static void toggleMarked(int label) {
+        int index = label - 1;
+        Task currTask = taskList.get(index);
+        currTask.markDone();
+        System.out.println(BREAK + "Nice! I've marked this task as done!\n" + currTask.statusIcon() + " " +
+                currTask.description + "\n" + BREAK);
+    }
+
+    public static void toggleUnmarked(int label) {
+        int index = label - 1;
+        Task currTask = taskList.get(index);
+        currTask.markUndone();
+        System.out.println(BREAK + "Okay, I have marked this task as undone!\n" + currTask.statusIcon() + " " +
+                currTask.description + "\n" + BREAK);
     }
 
     public static void main(String[] args) {
@@ -34,7 +52,16 @@ public class Julie {
                 showTask(taskList);
                 System.out.print(BREAK);
             } else {
-                addTask(input);
+                String[] parts = input.split(" ");
+                if (parts.length == 2 && parts[0].equals("mark")) {
+                    int label = Integer.parseInt(parts[1]);
+                    toggleMarked(label);
+                } else if (parts.length == 2 && parts[0].equals("unmark")) {
+                    int label = Integer.parseInt(parts[1]);
+                    toggleUnmarked(label);
+                } else {
+                    addTask(input);
+                }
             }
         }
         System.out.println(BREAK + EXIT + BREAK);
