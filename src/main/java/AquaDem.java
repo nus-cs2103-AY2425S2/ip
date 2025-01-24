76dd7816f2c60d25baf9877670d4909c42c79426
Vishesh Gauhar <>
2025-01-24 15:04:36 +0800

    Added some abstraction and implemented Level-1

--- src/main/java/AquaDem.java
+++ src/main/java/AquaDem.java
@@ -1,11 +1,34 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class AquaDem {
+    public AquaDem(){
+
+    }
+    private static final String bar = "____________________________________________________________";
+    private String intro() {
+        String str = bar + "\n" +  "Hello! I'm AquaDem \n" +
+                "What can I do for you?\n" + bar + "\n";
+        return str;
+    }
+
+    private void running() {
+
+        Scanner inputter = new Scanner(System.in);
+        String input = inputter.nextLine();
+        while (!Objects.equals(input, "bye")) {
+            System.out.println(bar);
+            System.out.println(input + "\n");
+            System.out.println(bar);
+            input = inputter.nextLine();
+        }
+        System.out.println(bar);
+        System.out.println("Cya again soon ;)" + "\n");
+        System.out.println(bar);
+    }
+
     public static void main(String[] args) {
-        String bar = "____________________________________________________________";
-        System.out.println(bar);
-        System.out.println("Hello! I'm AquaDem \n" +
-                "What can I do for you?");
-        System.out.println(bar);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(bar+ "\n");
+        AquaDem chatbot = new AquaDem();
+        System.out.print((chatbot.intro()));
+        chatbot.running();
     }
 }
