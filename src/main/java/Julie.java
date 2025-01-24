d518f2d65cce567954a886abc3eba6933796dc96
patil-np <nidhi.npatil@outlook.com>
2025-01-25 00:50:09 +0800

    Refactor main method for improved readability
    Main method is bulky, which affects the clarity of code.
    
    Let's extract task-specific logic and create separate methods, namely:
    * addToDo
    * addDeadline
    * addEvent
    for the main method to focus solely on control flow.

--- src/main/java/Julie.java
+++ src/main/java/Julie.java
@@ -7,12 +7,6 @@ public class Julie {
     private static final String EXIT = "Bye. See you later!\n";
     private static final ArrayList<Task> taskList = new ArrayList<>();
 
-//    public static void addTask(String input) {
-//        Task task = new Task(input);
-//        taskList.add(task);
-//        System.out.println(BREAK + "added: " + input + "\n" + BREAK);
-//    }
-
     public static void showTask(ArrayList<Task> list) {
         System.out.println("Here are your tasks:");
         for (int i = 0; i < list.size(); i++) {
@@ -47,6 +41,29 @@ public class Julie {
                 "Now you have " + taskList.size() + " tasks in the list.\n" + BREAK );
     }
 
+    public static void addToDo(String[] input) {
+        Task todo = new ToDo(mergeToString(input, 1, input.length));
+        taskList.add(todo);
+        ackMessage(todo);
+    }
+
+    public static void addDeadline(String input) {
+        String[] getDeadline = input.split("/by ");
+        String desc = getDeadline[0].substring(9, getDeadline[0].length());
+        Task deadline = new Deadline(desc, getDeadline[1]);
+        taskList.add(deadline);
+        ackMessage(deadline);
+    }
+
+    public static void addEvent(String input) {
+        String[] getEvent = input.split("/from ");
+        String desc = getEvent[0].substring(6, getEvent[0].length());
+        String[] getTime = getEvent[1].split("/to ");
+        Task event = new Event(desc, getTime[0], getTime[1]);
+        taskList.add(event);
+        ackMessage(event);
+    }
+
     public static void main(String[] args) {
         System.out.println(BREAK + INTRO + BREAK);
 
@@ -70,23 +87,11 @@ public class Julie {
                     int label = Integer.parseInt(parts[1]);
                     toggleUnmarked(label);
                 } else if (parts[0].equals("todo")) {
-                    Task todo = new ToDo(mergeToString(parts, 1, parts.length));
-                    taskList.add(todo);
-                    ackMessage(todo);
+                    addToDo(parts);
                 } else if (parts[0].equals("deadline")) {
-                    String[] getDeadline = input.split("/by ");
-                    String desc = getDeadline[0].substring(9, getDeadline[0].length());
-                    Task deadline = new Deadline(desc, getDeadline[1]);
-                    taskList.add(deadline);
-                    ackMessage(deadline);
-
+                    addDeadline(input);
                 } else if (parts[0].equals("event")) {
-                    String[] getEvent = input.split("/from ");
-                    String desc = getEvent[0].substring(6, getEvent[0].length());
-                    String[] getTime = getEvent[1].split("/to ");
-                    Task event = new Event(desc, getTime[0], getTime[1]);
-                    taskList.add(event);
-                    ackMessage(event);
+                    addEvent(input);
                 }
             }
         }
