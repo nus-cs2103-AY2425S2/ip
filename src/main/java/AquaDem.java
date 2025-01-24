942c61299e92cf3920de93de99ad6f790c9e3c1f
Vishesh Gauhar <>
2025-01-24 14:07:09 +0800

    Renaming main class and finishing Level-0

---
+++ src/main/java/AquaDem.java
@@ -0,0 +1,11 @@
+public class AquaDem {
+    public static void main(String[] args) {
+        String bar = "____________________________________________________________";
+        System.out.println(bar);
+        System.out.println("Hello! I'm AquaDem \n" +
+                "What can I do for you?");
+        System.out.println(bar);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(bar+ "\n");
+    }
+}
