052d97f393065ef2e4b2ff234f9f6630e9b9fd15
noahang <noahangshihern@gmail.com>
2025-02-13 12:09:46 +0800

    add junit tests, create jar file

---
+++ src/test/java/backend/ToDoListTest.java
@@ -0,0 +1,13 @@
+package backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ToDoListTest {
+    @Test
+    public void dummyTest() {
+        assertEquals(2,2);
+    }
+
+}
