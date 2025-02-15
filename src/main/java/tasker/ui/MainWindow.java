3946cc72d12fe3f1031c776dfed8c1d6eac7d214
BladerX11 <BladerX11@proton.me>
2025-02-15 19:15:16 +0800

    Refactor: Remove unused imports

--- src/main/java/tasker/ui/MainWindow.java
+++ src/main/java/tasker/ui/MainWindow.java
@@ -1,7 +1,6 @@
 package tasker.ui;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
