7696701af323aee1c573684c226e2a186a72e681
Jeron Kor <e0959034@u.nus.edu>
2025-02-28 18:21:03 +0800

    Refactor build configuration and update main entry point

--- src/main/java/lechatbot/Launcher.java
+++ src/main/java/lechatbot/Launcher.java
@@ -1,4 +1,13 @@
 package lechatbot;
 
+import javafx.application.Application;
+import lechatbot.ui.Main;
+
+/**
+ * A launcher class to workaround classpath issues.
+ */
 public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(Main.class, args);
+    }
 }
